26ca2951f2cd9189c81f7ffeabef3a5b52aff159
Kevin <kevin5112@yahoo.com>
2019-09-09 19:42:03 -0700

    initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,33 @@
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+// for "on" can only be used on objects that exist when the page loads...
+// gotta basically use it on it's parent..
+// add the child as the second argument after "click"
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		// grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+
+		// create a new li and add to ul
+		$("ul").append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + "</li>");
+	}
+});
+
+
+$(".fa-plus").click(function() {
+	$("input[type='text'").fadeToggle();
+});
+
